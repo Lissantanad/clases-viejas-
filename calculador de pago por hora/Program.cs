4516e4a8505e7052aabad19115da61fd57b22726
Lissantanad <153249528+Lissantanad@users.noreply.github.com>
2024-02-18 22:06:52 -0400

    Agregar archivos de proyecto.

---
+++ calculador de pago por hora/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese el pago por hora:");
+        double pagoPorHora = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese las horas trabajadas:");
+        int horasTrabajadas = Convert.ToInt32(Console.ReadLine());
+
+        double sueldoBruto = pagoPorHora * horasTrabajadas;
+        double descuentoAFP = sueldoBruto * 0.0285;
+        double descuentoSFS = sueldoBruto * 0.0304;
+        double totalDescuento = descuentoAFP + descuentoSFS;
+        double sueldoNeto = sueldoBruto - totalDescuento;
+
+        Console.WriteLine("Sueldo Bruto: " + sueldoBruto);
+        Console.WriteLine("Descuento de AFP: " + descuentoAFP);
+        Console.WriteLine("Total Descuento: " + totalDescuento);
+        Console.WriteLine("Sueldo Neto: " + sueldoNeto);
+    }
+}
